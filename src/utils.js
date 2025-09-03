bd6b26a7f657f030900d41c31894ad7980550ad9
biology009 <bipotitanic@proton.me>
2025-09-03 17:47:16 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,6 @@
-const SECRET_KEY = env.SECRET_KEY; // You can also move this to Wrangler [vars]
-
-export async function generateHMAC(data) {
-  const enc = new TextEncoder();
-  const keyData = enc.encode(data + SECRET_KEY);
-  const hashBuffer = await crypto.subtle.digest("SHA-256", keyData);
+export async function generateHMAC(data, secret) {
+  const enc = new TextEncoder().encode(data + secret);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", enc);
   return Array.from(new Uint8Array(hashBuffer))
     .map(b => b.toString(16).padStart(2, '0'))
     .join('');
