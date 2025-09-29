090fb24a0d63b59cf02d60da78f111cb61a51737
biology009 <bipotitanic@proton.me>
2025-09-30 01:15:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("verifyBtn").addEventListener("click", async () => {
   grecaptcha.ready(async () => {
-    const token = await grecaptcha.execute("your_site_key", { action: "submit" });
+    const token = await grecaptcha.execute("6Lc2LtkrAAAAAJbXj3HbkYzYT55rwX9TgwdGTI4r", { action: "submit" });
 
     // Send token + current URL token param to backend for verification
     const urlParams = new URLSearchParams(window.location.search);
